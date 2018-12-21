ec192d3101a55d821d96f033e9db0f054f7c7f80
gorkr <17320010710@163.com>
2018-12-21 11:52:51 +0800

    place doctest

--- flupy/1-2.py
+++ flupy/1-2.py
@@ -1,7 +1,7 @@
 #熟悉使用doctest
 #一个简单的二维向量类
 
-""
+"""
 %s是将变量传到str()函数中，结果是将变量转化适合人阅读的格式
 
 %r是将变量穿到repr()函数中，结果是将变量转化成适合机器阅读的格式
@@ -10,20 +10,23 @@
 from math import hypot
 # hypot 返回欧几里德范数 sqrt(x*x + y*y)。
 
+'''
+测试类功能时,doctest放在类外
+>>>v1 = Vector(2, 4)
+>>>v2 = Vector(2, 1)
+>>>v2 + v1
+Vector(4, 5)
+>>>v = Vector(3, 4)
+>>>abs(v)
+5.0
+>>>v * 3
+Vector(9, 12)
+>>>abs(v * 3)
+15.0
+'''
+
 class Vector:
-    '''
-    >>>v1 = Vector(2, 4)
-    >>>v2 = Vector(2, 1)
-    >>>v2 + v1
-    Vector(4, 5)
-    >>>v = Vector(3, 4)
-    >>>abs(v)
-    5.0
-    >>>v * 3
-    Vector(9, 12)
-    >>>abs(v * 3)
-    15.0
-    '''
+
 
     def __init__(self, x=0, y=0):
         self.x = x
